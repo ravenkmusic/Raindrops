d96af6c17421092564b51e5ea6e9e8328ec6ac9f
Raven Hendricks <ravenkbeats@gmail.com>
2026-03-02 22:48:07 -0500

    removed all elif statements

--- raindrops.py
+++ raindrops.py
@@ -3,9 +3,9 @@ def convert(number):
     
     if number % 3 == 0:
         result = "Pling"
-    elif number % 5 == 0:
+    if number % 5 == 0:
         result =+ "Plang"
-    elif number % 7 == 0:
+    if number % 7 == 0:
         result =+ "Plong"
     else:
        result = str(number)
