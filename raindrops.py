6a653378500a8e0f36e17e846452c51d951e7a33
Raven Hendricks <ravenkbeats@gmail.com>
2026-03-02 22:57:15 -0500

    added new if statement and unnested previous if statements

--- raindrops.py
+++ raindrops.py
@@ -3,10 +3,10 @@ def convert(number):
     
     if number % 3 == 0:
         result = "Pling"
-        if number % 5 == 0:
-            result += "Plang"
-            if number % 7 == 0:
-                result += "Plong"
-    else:
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    if result == "":
        result = str(number)
     return result
