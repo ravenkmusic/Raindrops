35630148a57163401f51c576f7bc30663a62adeb
Raven Hendricks <ravenkbeats@gmail.com>
2026-03-02 21:35:40 -0500

    fixed elif conditions

---
+++ raindrops.py
@@ -0,0 +1,20 @@
+def convert(number):
+    result = ""
+    
+    if number % 3 == 0 :
+        result = "Pling"
+        if number % 5 == 0:
+            result = "PlingPlang"
+        if number % 7 == 0:
+            result = "PlingPlangPlong"
+    elif number % 5 == 0:
+        result = "Plang"
+        if number % 7 == 0:
+            result = "PlangPlong"
+    elif number % 7 == 0:
+        result = "Plong"
+        if number % 3 == 0:
+            result = "PlingPlong"
+    else:
+       result = str(number)
+    return result
