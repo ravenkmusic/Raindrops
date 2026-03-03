eff637d6eb543bc629559d1f37e2b9e7f705ba81
Raven Hendricks <ravenkbeats@gmail.com>
2026-03-02 22:01:55 -0500

    replaced elif

--- raindrops.py
+++ raindrops.py
@@ -7,11 +7,11 @@ def convert(number):
             result = "PlingPlang"
         if number % 7 == 0:
             result = "PlingPlangPlong"
-    if number % 5 == 0:
+    elif number % 5 == 0:
         result = "Plang"
         if number % 7 == 0:
             result = "PlangPlong"
-    if number % 7 == 0:
+    elif number % 7 == 0:
         result = "Plong"
         if number % 3 == 0:
             result = "PlingPlong"
